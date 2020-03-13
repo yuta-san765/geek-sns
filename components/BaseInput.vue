b1675af0205a43f363079145614196f2c6382bad
yuta-san765 <nitoplus765@gmail.com>
2020-03-13 18:32:40 +0900

    BaseInputを追加

---
+++ components/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" v-bind="$attrs" @input="updateValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      default: null
+    }
+  },
+  methods: {
+    updateVaue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style></style>
