4f925eb902e757bdac30a61efaecb0a421f12457
yuta-san765 <nitoplus765@gmail.com>
2020-03-15 15:04:00 +0900

    Create TheFooter.vue

---
+++ components/TheFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <font-awesome-icon class="footer__icon" icon="home"> </font-awesome-icon>
+    <font-awesome-icon
+      class="footer__icon"
+      icon="comment-dots"
+    ></font-awesome-icon>
+    <font-awesome-icon class="footer__icon" icon="user"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.footer {
+  padding: 15px 0;
+  display: flex;
+  justify-content: space-around;
+  border-top: 1px solid $border-color;
+  &__icon {
+    color: $main-color;
+    font-size: 24px;
+  }
+}
+</style>
---
+++ pages/timeline.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <TheHeader></TheHeader>
+    <Post></Post>
+    <TheFooter></TheFooter>
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+import Post from '@/components/Post'
+import TheFooter from '@/components/TheFooter'
+export default {
+  components: {
+    TheHeader,
+    Post,
+    TheFooter
+  }
+}
+</script>
+
+<style lang="scss"></style>
